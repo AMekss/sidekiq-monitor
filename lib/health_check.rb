a896acc80daf307c3db70d1afb11ed977ca4e0c0
Artūrs Mekšs <arturs.mekss@gmail.com>
2018-06-11 20:01:02 +0300

    Properly handle redis unavailability

--- lib/health_check.rb
+++ lib/health_check.rb
@@ -1,10 +1,14 @@
 # frozen_string_literal: true
 require "sidekiq"
+require "logger"
 
 # Rack middleware for service health checks
 class HealthCheck
+  class UnsuccessfulRedisPing < StandardError; end
+
   HTTP_GET = 'GET'
   HTTP_OK = 200
+  HTTP_ERROR = 500
 
   HEALTH_PATH = '/healthz'
   HEALTH_HEADERS = {
@@ -18,20 +22,27 @@ class HealthCheck
 
   def initialize(app)
     @app = app
+    @logger = ::Logger.new(STDOUT)
   end
 
   def call(env)
     applicable?(env) ? health_response : @app.call(env)
+  rescue => err
+    @logger.error(err)
+    [HTTP_ERROR, HEALTH_HEADERS, [STATE_RED]]
   end
 
   private
 
   def health_response
-    [HTTP_OK, HEALTH_HEADERS, [health_body]]
+    check_redis!
+    [HTTP_OK, HEALTH_HEADERS, [STATE_GREEN]]
   end
 
-  def health_body
-    Sidekiq.redis(&:ping) == PING_SUCCESS ? STATE_GREEN : STATE_RED
+  def check_redis!
+    result = Sidekiq.redis(&:ping)
+    msg = "expect to have #{PING_SUCCESS}, but #{result} was received"
+    raise UnsuccessfulRedisPing, msg if result != PING_SUCCESS
   end
 
   def applicable?(env)
